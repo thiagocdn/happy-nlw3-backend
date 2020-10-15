0070da7125c662f5bd6129ab24fa462e9e297778
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-10-15 12:18:47 -0300

    Created Orphanage Class in Typeorm

--- src/database/migrations/1602724586815-CreateOrphanages.ts
+++ src/database/migrations/1602724586815-CreateOrphanages.ts
@@ -39,6 +39,10 @@ export class CreateOrphanages1602724586815 implements MigrationInterface {
 					type: 'text',
 				},
 				{
+					name: 'opening_hours',
+					type: 'varchar',
+				},
+				{
 					name: 'open_on_weekends',
 					type: 'boolean',
 					default: false,
